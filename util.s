23fac048e156b641259106bf670af0f4b2997b08
James Tan <jamestansx@gmail.com>
2024-01-27 13:15:16 +0800

    refactor: reduce LOC

--- util.s
+++ util.s
@@ -14,8 +14,7 @@ SetPinVal       PROC
 ClearPinVal     PROC
                 PUSH    {R4, LR}
                 LDR     R4, [R0]
-                MVN     R1, R1
-                AND     R4, R4, R1
+                BIC     R4, R4, R1
                 STR     R4, [R0]
                 POP     {R4, LR}
                 BX      LR
@@ -35,10 +34,7 @@ IsBitSet        PROC
 DelayLong       PROC
                 PUSH    {R4, LR}
                 ; TODO: make the delay time configurable
-                ; currently the delay time is arbitrary
-                ; I have no idea how long this would take
-                ; because I don't know how to get the clock
-                ; frequency of processor
+                ; ref: https://github.com/arduino/ArduinoCore-samd/blob/993398cb7a23a4e0f821a73501ae98053773165b/cores/arduino/delay.c
                 MOV     R4, #0xFFFF
                 MOVT    R4, #0x0007
 while_delayLong SUBS    R4, R4, #1
@@ -51,10 +47,7 @@ while_delayLong SUBS    R4, R4, #1
 DelayShort      PROC
                 PUSH    {R4, LR}
                 ; TODO: make the delay time configurable
-                ; currently the delay time is arbitrary
-                ; I have no idea how long this would take
-                ; because I don't know how to get the clock
-                ; frequency of processor
+                ; ref: https://github.com/arduino/ArduinoCore-samd/blob/993398cb7a23a4e0f821a73501ae98053773165b/cores/arduino/delay.c
                 MOV     R4, #0xFFFF
                 MOVT    R4, #0x0000
 while_delayS	SUBS    R4, R4, #1
